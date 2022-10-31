fadcb43413fe6e6a7da5d7d2ddbf71ef5054e6b1
Azamat <aza@MacBook-Pro-Azamat.local>
2022-10-31 18:06:13 +0600

    1st

---
+++ app/src/main/java/com/example/cryptocom/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cryptocom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
